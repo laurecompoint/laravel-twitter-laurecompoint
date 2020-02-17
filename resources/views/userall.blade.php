35c3c2c965063ebbe8931908b311a39a5fbf0de1
LaureCompoint <compointlaure@gmail.com>
2020-02-17 17:43:54 +0100

    page user all ok

--- resources/views/userall.blade.php
+++ resources/views/userall.blade.php
@@ -2,15 +2,17 @@
 
 @section('content')
 <div class="bg-info d-flex justify-content-center align-items-center align-content-center" style="height: 70px"><h4 class="text-white ">Users</h4></div>
-<div class="mt-4 container row d-flex flex-wrap">
+
+<div class="">
+<div class="mt-4 container-fluid row m-auto" style="width: 95% ;">
 
 @foreach($usersall as $user)
 
 
-<div class="bg-white border ml-2 mt-2 w-25" style="height: 150px;  background: url('/img/{{ $user->avatar }}') no-repeat;  background-size: cover;  border-radius: 22px #665A5C;box-shadow: 4px 2px 4px #665A5C;">
+<div class="bg-white border mr-5  mt-2" style="height: 150px;width: 20% ; background: url('/img/{{ $user->avatar }}') no-repeat;  background-size: cover;  border-radius: 22px #665A5C;box-shadow: 4px 2px 4px #665A5C;">
                    
-                   <a href="{{ $user->username }}" class="text-info">
-                       <h4 class="list-group-item-heading">{{$user->name}}</h4>
+                   <a href="{{ $user->username }}" class="text-dark ">
+                       <h4 class="list-group-item-heading font-weight-bold">{{$user->name}}</h4>
                        <small class="list-group-item-text">@ {{$user->username}}</small>
                    </a>
               
@@ -20,5 +22,6 @@
 @endforeach
                        
    
+</div>
 </div>
 @endsection
